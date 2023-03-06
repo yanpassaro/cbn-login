2d3e4e758e18a3c83f7404945c123a04a7be3c1a
Yan <yannpassaro@gmail.com>
2023-03-06 20:14:45 -0300

    initial commit

---
+++ src/test/java/com/cbn/login/CbnLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbn.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbnLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
